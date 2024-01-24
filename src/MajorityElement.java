5c0ce4eb9d9208fd5b032759e7c432628030f913
vinaykumarranjan <vinaykumarranjan83@gmail.com>
2024-01-24 23:56:00 +0530

    majorityElement in an array

---
+++ src/MajorityElement.java
@@ -0,0 +1,20 @@
+public class MajorityElement {
+    public static int majorityElement(int []nums) {
+        int element=-1,count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                element=nums[i];
+                count++;
+            } else if(element==nums[i]){
+                count++;
+            } else{
+                count--;
+            }
+        }
+        return element;
+    }
+    public static void main(String[] args) {
+        int res = majorityElement(new int[]{2, 2, 1, 3, 1, 1, 3, 1, 1,2,3,2,2,2});
+        System.out.println(res);
+    }
+}
