466118ad85ceb8d903b8c9dfc4bae2ea10d8ddce
vinaykumarranjan <vinaykumarranjan83@gmail.com>
2025-09-30 12:22:15 +0530

    Commit rod cutting

--- src/DP/RodCuttingProblem.java
+++ src/DP/RodCuttingProblem.java
@@ -37,4 +37,50 @@ public class RodCuttingProblem {
         //int[] price = {3, 5, 8, 9, 10, 17, 17, 20};//24
         System.out.println("maximum value obtainable by cutting up the rod and selling the pieces : "+rodCuttingProblem.cutRod(price));
     }
+
+    //using Tabulation-S(c)-O(n*n)
+    // public int cutRod(int[] price) {
+    //     // code here
+    //     int n = price.length;
+    //     int[][] dp = new int[n][n+1];
+    //     for(int j=0;j<=n;j++){
+    //         dp[0][j] = j*price[0];
+    //     }
+
+    //     for(int i=1;i<n;i++){
+    //         for(int j=0;j<=n;j++){
+    //             int notTake = 0 + dp[i-1][j];
+    //             int take = Integer.MIN_VALUE;
+    //             if(i+1<=j){
+    //                 take =price[i] + dp[i][j-(i+1)];
+    //             }
+    //             dp[i][j] = Math.max(take,notTake);
+    //         }
+    //     }
+    //     return dp[n-1][n];
+    // }
+
+    //using Tabulation-S(c)-O(n)
+    // public int cutRod(int[] price) {
+    //     // code here
+    //     int n = price.length;
+    //     int[] prev = new int[n+1];
+    //     for(int j=0;j<=n;j++){
+    //         prev[j] = j*price[0];
+    //     }
+
+    //     for(int i=1;i<n;i++){
+    //         int[] curr = new int[n+1];
+    //         for(int j=0;j<=n;j++){
+    //             int notTake = 0 + prev[j];
+    //             int take = Integer.MIN_VALUE;
+    //             if(i+1<=j){
+    //                 take =price[i] + curr[j-(i+1)];
+    //             }
+    //             curr[j] = Math.max(take,notTake);
+    //         }
+    //         prev=curr;
+    //     }
+    //     return prev[n];
+    // }
 }
